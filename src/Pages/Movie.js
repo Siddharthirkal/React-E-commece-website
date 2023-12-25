746533b6a49d4b915daac709d41d950a02ac27bf
Siddharthirkal <siddharthirkal06>
2023-12-25 12:31:51 +0530

    IsLoading the Application

--- src/Pages/Movie.js
+++ src/Pages/Movie.js
@@ -3,12 +3,17 @@ import React, { useState, useEffect } from 'react';
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
   useEffect(() => {
-    fetchMoviesHandler();
-  }, []);
+    if (buttonClicked) {
+      fetchMoviesHandler();
+    }
+  }, [buttonClicked]);
 
   const fetchMoviesHandler = () => {
+    setIsLoading(true);
     fetch('https://swapi.dev/api/films/')
       .then(response => response.json())
       .then(data => {
@@ -19,33 +24,49 @@ const Movie = () => {
           releaseDate: movieData.release_date
         }));
         setMovies(transformedMovies);
+        setIsLoading(false);
       })
-      .catch(error => console.error('Error fetching movies:', error));
+      .catch(error => {
+        console.error('Error fetching movies:', error);
+        setIsLoading(false);
+      });
+  };
+
+  const handleButtonClick = () => {
+    setButtonClicked(true);
   };
 
   return (
     <div>
-      <button onClick={fetchMoviesHandler} className="btn btn-primary my-3">Fetch Movies</button>
-      <table className="table table-bordered table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Opening Text</th>
-            <th>Release Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {movies.map(movie => (
-            <tr key={movie.id}>
-              <td>{movie.id}</td>
-              <td>{movie.title}</td>
-              <td>{movie.openingText}</td>
-              <td>{movie.releaseDate}</td>
+      <button onClick={handleButtonClick} className="btn btn-primary my-3">
+        Fetch Movies
+      </button>
+      {!buttonClicked && <p>No movies found.</p>}
+
+      {isLoading && <p>Loading...</p>}
+
+      {!isLoading && movies.length > 0 && (
+        <table className="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Opening Text</th>
+              <th>Release Date</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie.id}>
+                <td>{movie.id}</td>
+                <td>{movie.title}</td>
+                <td>{movie.openingText}</td>
+                <td>{movie.releaseDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
